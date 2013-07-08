83f9685418a77e3d5409dd88e2dddb2d0eb73264
Deneb Garza <denebgarza@gmail.com>
2013-07-08 00:19:59 -0500

    Fixed last_check error
    Error was caused by last check not being initialized.
    Also changed it to use getField.

--- AppUser.php
+++ AppUser.php
@@ -10,30 +10,30 @@
   {
     private $db = null;
     private $join_time = 0;
-    private $last_check;
+    private $last_check = 0;
     
     function __construct($id, &$db) {
       $last_check = time();
       if($id && $db) {
         $this->db = $db;
         $this->db->setQuery('SELECT * FROM users WHERE user_id = '.$id.' LIMIT 1');
-        $this->id = $this->db->getResult(0, 'user_id');
-
+        $this->id = $this->db->getField('user_id');
+        
         if($this->id) { // If the user exists in the database
           // $this->name = $db->getResult($q, 0, 'name');
-          $this->friends_json = $this->db->getResult(0, 'user_friends_json'); // ### REMOVE 'user_' ###
+          $this->friends_json = $this->db->getField('user_friends_json'); // ### REMOVE 'user_' ###
           $this->friends_array = json_decode($this->friends_json, true);
         }
       }
     }
     
     public function getLastCheck() {
-      if(!$this->last_check) $this->last_check = $this->db->getResult(0, 'last_check');
+      if(!$this->last_check) $this->last_check = $this->db->getField('last_check');
       return $this->last_check;
     }
     
     public function getJoinTime() {
-      if(!$this->join_time) $this->join_time = $this->db->getResult(0, 'time');
+      if(!$this->join_time) $this->join_time = $this->db->getField('time');
       return $this->join_time;
     }
     
